099f5473702a07c59dbfb6b5936c2ca8cf8e1d1d
MishaGalin <mishagalin85@gmail.com>
2025-01-20 03:28:20 +0500

    some logic changes

--- brightness_control.py
+++ brightness_control.py
@@ -12,16 +12,16 @@ async def main():
     default_latitude = None
     default_longitude = None
     default_adaptive_brightness = False
+    default_update_interval = 1.0
 
-    parser = ArgumentParser(
-        description="Brightness control based on sunrise and sunset."
-    )
+    parser = ArgumentParser()
     parser.add_argument("--min", type=int, default=default_min_brightness)
     parser.add_argument("--max", type=int, default=default_max_brightness)
     parser.add_argument("--speed", type=float, default=default_change_speed)
     parser.add_argument("--lat", type=float, default=default_latitude)
     parser.add_argument("--lng", type=float, default=default_longitude)
     parser.add_argument("--adapt", type=bool, default=default_adaptive_brightness)
+    parser.add_argument("--interval", type=float, default=default_update_interval)
     args = parser.parse_args()
 
     brightness_min = args.min
@@ -30,6 +30,7 @@ async def main():
     latitude = args.lat
     longitude = args.lng
     adaptive_brightness = args.adapt
+    update_interval = args.interval
 
     if not (0 <= brightness_min <= 100):
         raise ValueError("Minimum brightness must be between 0 and 100.")
@@ -38,14 +39,18 @@ async def main():
     if not (brightness_min < brightness_max):
         raise ValueError("Minimum brightness must be less than maximum brightness.")
     controller = BrightnessController(
-        brightness_min, brightness_max, change_speed, adaptive_brightness
+        brightness_min,
+        brightness_max,
+        change_speed,
+        adaptive_brightness,
+        update_interval,
     )
 
     if latitude is None or longitude is None:
         latitude, longitude = get_coordinates()
     location = LocationInfo(latitude=latitude, longitude=longitude)
 
-    await controller.start_main_loop(location, 2.0)
+    await controller.start_main_loop(location)
 
 
 asyncio.run(main())
