0cc4107ce4f1257f993de22ee3cb0b5d94f87234
MishaGalin <mishagalin85@gmail.com>
2025-02-17 02:11:10 +0500

    create_task.bat now creates two tasks: one to run the app on pc startup, the other to run the app when the pc wakes up.

--- create_task.bat
+++ create_task.bat
@@ -15,19 +15,22 @@ if not exist "%exePath%" (
 )
 
 :: Task name
-set taskName=BrightnessControlTask
+set taskOnStartUpName=AutoBrightnessControlTaskOnStartUp
+set taskOnWakeUpName=AutoBrightnessControlTaskOnWakeUp
 
 :: Delete existing task with the same name (if any)
-schtasks /delete /tn "%taskName%" /f >nul 2>&1
+schtasks /delete /tn "%taskOnStartUpName%" /f >nul 2>&1
+schtasks /delete /tn "%taskOnWakeUpName%" /f >nul 2>&1
 
 :: Create a new task
-schtasks /create /tn "%taskName%" /tr "\"%exePath%\" %args%" /sc onlogon /rl highest >nul 2>&1
+schtasks /create /tn "%taskOnStartUpName%" /tr "\"%exePath%\" %args%" /sc onlogon /rl highest >nul 2>&1
+schtasks /create /ru "SYSTEM" /sc onevent /mo "*[System[Provider[@Name='Microsoft-Windows-Kernel-Power'] and EventID=107]]" /ec System /tn "%taskOnWakeUpName%" /tr "\"%exePath%\" %args%"
 
 :: Check if the task was created successfully
 if %errorlevel% equ 0 (
     echo Task successfully created and will run AutoBrightnessControl.exe at system logon.
     echo Running the created task now...
-    schtasks /run /tn "%taskName%"
+    schtasks /run /tn "%taskOnStartUpName%"
 ) else (
     echo Error while creating the task. Try running with administrator rights.
 )
